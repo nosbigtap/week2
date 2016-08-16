c37a0b1da07050993c3309b47fdf9c7a945031c6
Patrick Gibson <nosbigtap@gmail.com>
2016-08-16 19:53:07 +0000

    SV

--- SV.js
+++ SV.js
@@ -1,11 +1,4 @@
 var userInput = prompt("Please enter your phone number with dashes.")
 
-function validateForm() {
-    if {
-        userInput.length!=12 && userInput.charAt(3) != "-" && userInput.charAt(7) != "-"
-        alert("Name must be filled out");
-        return false;
-    }
-    
 
-}
+
