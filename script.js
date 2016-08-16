7c330fdbbfa81a072114f876037e050490ba7ec9
Patrick Gibson <nosbigtap@gmail.com>
2016-08-16 21:57:45 +0000

    stringReporter

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
 var name = prompt("Please enter your name", "YOUR NAME")
 
 alert(name + "\n" + name.length + "\n" + name.charAt(1) + "\n" + name.toLowerCase() + "\n" + name.toUpperCase() + "\n" + "You entered " + name + "\n" + name.substring(1, 4));
+
